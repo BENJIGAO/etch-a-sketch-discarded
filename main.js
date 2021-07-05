fa7c468a3731ce3f4b1a1cce9a216c7560051bb4
Brian Gao <bhgao20014@gmail.com>
2021-07-05 12:14:50 -0400

    Made it so when user clicks and holds, grid changes colour (only this case)

--- main.js
+++ main.js
@@ -4,10 +4,35 @@ function createGrid(sideLength) {
     console.log(sideMeasure);
     gridContainer.style.cssText = `grid-template-columns: ${sideMeasure}; grid-template-rows: ${sideMeasure};`;
     for (let i = 0; i < sideLength**2; i++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.classList.add('grid-square');
-        gridContainer.appendChild(gridDiv);
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridContainer.appendChild(gridSquare);
     }
 }
 
+function addBlack(e) {
+    e.target.classList.add('black');
+}
+
+function addEventListeners() {
+    const body = document.body;
+    
+    body.addEventListener('mousedown', () => {
+        const gridSquares = document.querySelectorAll('.grid-square');
+        gridSquares.forEach((gridSquare) => {
+            gridSquare.addEventListener('mouseenter', addBlack);
+        })
+    })
+    body.addEventListener('mouseup', () => {
+        const gridSquares = document.querySelectorAll('.grid-square');
+        gridSquares.forEach((gridSquare) => {
+            gridSquare.removeEventListener('mouseenter', addBlack)
+        })
+    })
+}
+
+
+addEventListeners();
+
 createGrid(16);
+
