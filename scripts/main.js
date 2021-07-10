63e6109210e8d0e04e42a28ac3467f7d6e2c5292
Brian Gao <bhgao20014@gmail.com>
2021-07-10 16:28:04 -0400

    Finished cleaning up js file; I'm done; yaygit status!

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-let isMouseDown = 0;
+let isMouseDown = 0; /* Global b/c accessed in multiple other functions */
 
 monitorMouseStatus();
 
@@ -15,7 +15,8 @@ activateBtns();
 /* First Major Function */
 function monitorMouseStatus() {
     const body = document.body;
-    body.addEventListener('mousedown', () => isMouseDown++, {capture: true});
+    /* 'capture: true' done so isMouseDown changes first so other functions can run properly (when mouse is pressed) */
+    body.addEventListener('mousedown', () => isMouseDown++, {capture: true}); 
     body.addEventListener('mouseup', () => isMouseDown--);
 }
 
@@ -24,6 +25,7 @@ function createGrid(sideLength) {
     const gridContainer = document.getElementById('grid-container');
     let sideMeasure = '1fr '.repeat(sideLength);
     gridContainer.style.cssText = `grid-template-columns: ${sideMeasure}; grid-template-rows: ${sideMeasure};`;
+    /* grid is square (multiply by 2) */
     for (let i = 0; i < sideLength**2; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
@@ -35,7 +37,7 @@ function createGrid(sideLength) {
 function manageGridSizeDisplay() {
     const display = document.getElementById('grid-size-display');
     const slider = document.getElementById('grid-size-slider');
-    display.textContent = `${slider.value}`
+    display.textContent = `${slider.value}`;
     slider.addEventListener('input', updateDisplay);
 }
 
@@ -44,12 +46,12 @@ function updateDisplay(e) {
     display.textContent = `${e.target.value}`;
 }
 
+/* Fourth Major Function */
 function changeGridSize() {
     const slider = document.getElementById('grid-size-slider');
-    slider.addEventListener('change', updateGrid)
+    slider.addEventListener('change', updateGrid);
 }
 
-/* Fourth Major Function */
 function updateGrid(e) {
     clearDivs();
     createGrid(e.target.value);
@@ -66,7 +68,8 @@ function clearDivs() {
 /* Fifth Major Function */
 function makeGridResponsive() {
     const gridSquares = document.querySelectorAll('.grid-square');
-    const tmpRef = document.querySelector('.selected')
+    const tmpRef = document.querySelector('.selected');
+    /* Whichever tool is selected, 'callback' is the tool's related function */
     const callback = window[`add${tmpRef.id.slice(0, 1).toUpperCase() + tmpRef.id.slice(1, -4)}`];
     gridSquares.forEach((gridSquare) => {
         gridSquare.addEventListener('mouseenter', callback);
@@ -78,7 +81,7 @@ function makeGridResponsive() {
 function activateBtns() {
     const clearBtn = document.getElementById('clear-btn');
     clearBtn.addEventListener('click', clearGrid);
-    
+
     /* For color and eraser button(s) */
     const toolBtns = document.querySelectorAll('.tool');
     toolBtns.forEach((toolBtn) => {
@@ -97,12 +100,13 @@ function clearGrid() {
 function initiateTool() {
     const unwantedTool = document.querySelector('.selected');
     unwantedTool.classList.remove('selected');
+    /* string of function name of deselected tool */
     let tmpstring = `add${unwantedTool.id.slice(0, 1).toUpperCase() + unwantedTool.id.slice(1, -4)}`;
     let unwantedFunction = window[tmpstring];
     this.classList.add('selected');
+    /* string of function name of selected tool */
     tmpstring = `add${this.id.slice(0, 1).toUpperCase() + this.id.slice(1, -4)}`;
     let wantedFunction = window[tmpstring];
-    console.log(wantedFunction);
     const gridSquares = document.querySelectorAll('.grid-square');
     gridSquares.forEach((gridSquare) => {
         gridSquare.removeEventListener('mouseenter', unwantedFunction);
@@ -171,10 +175,5 @@ function addEraser(e) {
     if (isMouseDown) {
         e.target.style.backgroundColor = '';
         e.target.className = 'grid-square';
-    }
-    
+    } 
 }
-
-
-
-
